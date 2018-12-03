edaf7b0006b4a4278b181c9762e0061225a2e142
Tristan S <tristan.scheidemann@live.com>
2018-12-03 17:59:30 +0100

    Preparations 4 tutorial tomorrow

--- Documents/Uni/Programming/Machine Learning Tutorium/github Ordner/other interesting stuff/Programming Tutorial/diagnostics.py
+++ Documents/Uni/Programming/Machine Learning Tutorium/github Ordner/other interesting stuff/Programming Tutorial/diagnostics.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+
 class bias_variance_linear_regression:
 	#comparison to Bishop notation: mean_value=h(x), functions= y^(l)(x)
 	def __init__(self, functions, mean_value):
@@ -15,5 +16,8 @@ class bias_variance_linear_regression:
 		return (1/len(data))*np.sum([math.pow(self.mean_prediction(x)-self.mean_value(x),2) for x in data])
 		
 		
-		
+	def variance_regression(self,data):
+			return (1/len(data))*np.sum((1/(self.L)*np.sum(math.pow( self.functions[l].predict(np.array([x]))-self.mean_prediction(x),2) for l in range(self.L)) for x in data))
+			
+					
 		
